2884b6a5b65ed9ccd417990e2854c950689878f8
Jonathan Santos <jonathan.willian321@gmail.com>
2023-12-04 02:38:37 -0300

    day 2 task 1

--- day3/task1.py
+++ day3/task1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def main():
+    dot_or_number = ".0123456789"
     file = "input"
     lines = read_file(file).split("\n")
 
@@ -12,12 +13,17 @@ def main():
             if not inside_digit and c.isdigit():
                 inside_digit = True
                 digit += c
-            elif inside_digit and not c.isdigit():
-                maybe_parts.append((i, j-len(digit),digit))
-                digit = ""
-                inside_digit = False
             elif inside_digit:
-                digit += c
+                if not c.isdigit():
+                    maybe_parts.append((i, j-len(digit),digit))
+                    digit = ""
+                    inside_digit = False
+                elif j == len(line)-1:
+                    digit += c
+                    maybe_parts.append((i, j-len(digit)+1,digit))
+                else:
+                    digit += c
+
             else:
                 assert not c.isdigit()
 
@@ -27,23 +33,22 @@ def main():
         end_digit = start_digit + len(digit)-1
         line = lines[linei]
 
-
-        right_side = end_digit+1
-        left_side = start_digit-1
+        right_side = end_digit + 1
+        left_side = start_digit - 1
         linei_up = linei - 1
         linei_down = linei + 1
 
-        if right_side < len(line) and line[right_side] != '.':
+        if right_side < len(line)-1 and line[right_side] != '.':
             actual_parts.append(part)
         elif left_side >= 0 and line[left_side] != '.':
             actual_parts.append(part)
         else:
             for i in range(left_side, right_side+1, 1):
                 if i >= 0 and i < len(line):
-                    if linei_up >= 0 and lines[linei_up][i] != '.':
+                    if linei_up >= 0 and lines[linei_up][i] not in dot_or_number:
                         actual_parts.append(part)
                         break
-                    elif linei_down < len(lines) and lines[linei_down][i] != '.':
+                    elif linei_down < len(lines) and lines[linei_down][i] not in dot_or_number:
                         actual_parts.append(part)
                         break
 
@@ -51,6 +56,7 @@ def main():
     for part in actual_parts:
         _, _, digit = part
         total += int(digit)
+        print(part)
 
     print(total)
 
